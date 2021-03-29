6b599c0776ff39fb88cc66f6654b6f95bdd5fe01
Mattew05 <mateinr5@yahoo.ro>
2021-03-29 15:19:16 +0300

    First car prototype classes

---
+++ car-generating/CarGenerator.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class CarGenerator {
+    private int frequency;
+    public ArrayList<Car> cars;
+
+    public void generate(){
+        //todo: make cars go vroom
+        //in functie de frecventa pe fiecare strada vor fi generate un numar de masini(strict <= capacitatea strazii)
+    }
+
+    public CarGenerator(int frequency) {
+        cars=new ArrayList<Car>();
+        this.frequency = frequency;
+    }
+}
---
+++ car-generating/SpecialCar.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.*;
+
+public class SpecialCar extends Car{
+
+    public SpecialCar(int initialPosition, int finalPosition, int distance) {
+        this.initialPosition = initialPosition;
+        this.finalPosition = finalPosition;
+        this.speed = 0;
+        this.distance = distance;
+    }
+}
