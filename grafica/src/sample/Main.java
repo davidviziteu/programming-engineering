81482b42d1f71a689adf190c388369206e0e78b2
andreeabmanea <75753307+andreeabmanea@users.noreply.github.com>
2021-04-19 20:52:20 +0300

    Add files via upload

--- grafica/src/sample/Main.java
+++ grafica/src/sample/Main.java
@@ -4,16 +4,29 @@ import javafx.application.Application;
 import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Point2D;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main extends Application {
+    String input;
+    HBox user = new HBox();
+    Button submit = new Button("Submit");
+    Label trafficLabel = new Label("Select Traffic:");
+    ObservableList<String> trafficTypes = FXCollections.observableArrayList("Low","Medium","High");
+    Spinner<String> trafficSpinner = new Spinner<>();
 
     /* Matricea initiala data de echipa de la strazi
     public Integer[][] map={
@@ -47,10 +60,20 @@ public class Main extends Application {
     };
     int x = 0;
     int y = 200;
+    int mouseXstart;
+    int mouseYstart;
+    int mouseXfinal;
+    int mouseYfinal;
+    Point2D point;
+    Point2D start;
+    Point2D finish;
+    int countClick = 0;
+    List<Point2D> special = new ArrayList<>();
+    GridPane gridpane = new GridPane();
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws InterruptedException {
 
-        GridPane gridpane = new GridPane();
+
         Image streetBlock = new Image("/resources/StreetBlock.jpg");
         Image streetBlockUp = new Image("/resources/StreetBlockUp.jpg");
         Image grass = new Image("/resources/grass.png");
@@ -74,38 +97,62 @@ public class Main extends Application {
                 }
             }
 
-        HBox user = new HBox();
-        Label trafficLabel = new Label("Select Traffic:");
-        ObservableList<String> trafficTypes = FXCollections.observableArrayList("Low","Medium","High");
-        Spinner<String> trafficSpinner = new Spinner<>();
         SpinnerValueFactory<String> values = new SpinnerValueFactory.ListSpinnerValueFactory<>(trafficTypes);
         trafficSpinner.setValueFactory(values);
 
-        //Am creat un dreptunghi pentru a testa cum se misca ceva pe harta (care simuleaza o masina).
-        Rectangle car = new Rectangle(40, 20);
-        car.setTranslateY(185);
+        Image car = new Image("/resources/specialCarGoingRight.png");
+        ImageView carView = new ImageView(car);
+
+        carView.setTranslateY(125);
         Button move = new Button("Move");
 
         move.setOnAction(e->{
-            x+=20;
-            car.setTranslateX(x);
+            x+=10;
+            carView.setTranslateX(x);
 
         });
-        user.getChildren().addAll(trafficLabel, trafficSpinner, move);
+
+
+        user.getChildren().addAll(trafficLabel, trafficSpinner, submit, move);
         user.setAlignment(Pos.CENTER);
 
         BorderPane map = new BorderPane();
         map.setCenter(gridpane);
         map.setTop(user);
-        map.getChildren().add(car);
+        map.getChildren().add(carView);
         Scene scene = new Scene(map, 1000, 600);
-
         primaryStage.setScene(scene);
         primaryStage.show();
+        getInput();
+        getCoordForSpecialCar();
+    }
+
+    public String getInput(){
+
+        submit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                input = trafficSpinner.getValue();
+                System.out.println(input);
+            }
+        });
+        return input;
+    }
+
+    public Point2D getCoordForSpecialCar(){
+        gridpane.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                point = new Point2D(mouseEvent.getX(), mouseEvent.getY());
+
+            }
+        });
+        return point;
     }
 
     public static void main(String[] args) {
         launch(args);
+
     }
 }
 
