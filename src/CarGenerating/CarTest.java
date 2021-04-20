c7745412876dfb2d4bfc301d724b1c9089c0ac54
Teodorovici Gavril-Anton <teodorovici.gavril11@gmail.com>
2021-04-20 09:48:58 +0300

    fixed test methods

--- src/CarGenerating/CarTest.java
+++ src/CarGenerating/CarTest.java
@@ -1,24 +1,12 @@
 package CarGenerating;
 
+
 import org.junit.Assert;
-import org.testng.annotations.Test;
+import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
-
-
-    @Test
-    void setDistance() {
-    }
-
-    @Test
-    void testSetInitialPosition() {
-    }
-
-    @Test
-    void setFinalPosition() {
-    }
 
     @org.junit.jupiter.api.Test
     void setSpeed_5() {
@@ -34,6 +22,24 @@ class CarTest {
         car.setSpeed(50);
 
         Assert.assertEquals(50, car.getSpeed());
+    }
+
+    @org.junit.jupiter.api.Test
+    void setSpeed_150() {
+        Car car = new Car();
+        car.setSpeed(150);
+
+        Assert.assertEquals(150, car.getSpeed());
+    }
+
+    @org.junit.jupiter.api.Test
+    void setSpeed_0() {
+        Car car = new Car();
+        car.setSpeed(0);
+
+        Assert.assertEquals(0, car.getSpeed());
+    }
+
     @org.junit.jupiter.api.Test
     void getDistance() {
         Car car = new Car();
@@ -56,19 +62,5 @@ class CarTest {
 
     }
 
-    @org.junit.jupiter.api.Test
-    void setSpeed_150() {
-        Car car = new Car();
-        car.setSpeed(150);
 
-        Assert.assertEquals(150, car.getSpeed());
-    }
-
-    @org.junit.jupiter.api.Test
-    void setSpeed_0() {
-        Car car = new Car();
-        car.setSpeed(0);
-
-        Assert.assertEquals(0, car.getSpeed());
-    }
 }
