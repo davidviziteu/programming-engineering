bd1fc2b6b3f62ed03af34bf781a2d3d6d3b67c4a
Teodorovici Gavril-Anton <teodorovici.gavril11@gmail.com>
2021-04-20 08:54:03 +0300

    Test classes

---
+++ src/CarGenerating/CarGeneratorTest.java
@@ -0,0 +1,12 @@
+package CarGenerating;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarGeneratorTest {
+
+    @Test
+    void generate() {
+    }
+}
---
+++ src/CarGenerating/CarTest.java
@@ -0,0 +1,44 @@
+package CarGenerating;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    @Test
+    void setInitialPosition() {
+    }
+
+    @Test
+    void getInitialPosition() {
+    }
+
+    @Test
+    void getFinalPosition() {
+    }
+
+    @Test
+    void getSpeed() {
+    }
+
+    @Test
+    void getDistance() {
+    }
+
+    @Test
+    void setDistance() {
+    }
+
+    @Test
+    void testSetInitialPosition() {
+    }
+
+    @Test
+    void setFinalPosition() {
+    }
+
+    @Test
+    void setSpeed() {
+    }
+}
