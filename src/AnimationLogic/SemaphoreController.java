1567360bd26a4c29f328412faa72e2bf5cba6062
andreea1603 <62295059+andreea1603@users.noreply.github.com>
2021-04-26 21:49:59 +0300

    Add files via upload

--- src/AnimationLogic/SemaphoreController.java
+++ src/AnimationLogic/SemaphoreController.java
@@ -1,11 +1,41 @@
 package AnimationLogic;
 
+import CityGenerating.CityGenerator;
+import CityGenerating.TrafficLights;
+
+import static AnimationLogic.Miscellaneous.Utilities.existsACarOnStreets;
 
 
-public class SemaphoreController extends Thread {
+public class SemaphoreController extends Thread  {
+
+    SemaphoreController(){}
+
     @Override
     public void run() {
 
+        while (existsACarOnStreets()){
+
+            for(int i=0; i< CityGenerator.city.getTrafficLights().size(); i++) {
+                TrafficLights trafficLights=CityGenerator.city.getTLightsById(i);
+                if (trafficLights.getTimer() > 0) {
+                    trafficLights.setTimer(trafficLights.getTimer() - 1);
+                } else {
+                    trafficLights.setTimer(trafficLights.getMaxTime());
+                    trafficLights.setColor(trafficLights.getStare().next());
+                    trafficLights.setPoz((trafficLights.getPoz() + 1) % 4);
+                }
+            }
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Dupa o secunda: ");
+            for(int i=0; i< CityGenerator.city.getTrafficLights().size(); i++){
+                System.out.println(CityGenerator.city.getTLightsById(i));
+            }
+        }
         System.out.println("semaphore controller run");
     }
 }
