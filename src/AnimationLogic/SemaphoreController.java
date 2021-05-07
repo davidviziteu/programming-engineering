91af088125409db8055c77a120aae02cdb023af5
andreea1603 <62295059+andreea1603@users.noreply.github.com>
2021-05-07 21:59:12 +0300

    Update SemaphoreController.java

--- src/AnimationLogic/SemaphoreController.java
+++ src/AnimationLogic/SemaphoreController.java
@@ -10,31 +10,48 @@ public class SemaphoreController extends Thread  {
 
     SemaphoreController(){}
 
+    public static void change(TrafficLights trafficLights){
+        if (trafficLights.getTimer() > 0) {
+            trafficLights.setTimer(trafficLights.getTimer() - 1);
+        } else {
+
+            if(trafficLights.getStare().equals(TrafficLights.StareSemafor.Green) || trafficLights.getStare().equals(TrafficLights.StareSemafor.Red))
+            {
+                trafficLights.setTimer(trafficLights.getTimeMax2());
+            }
+            else
+                trafficLights.setTimer(trafficLights.getMaxTime());
+
+            trafficLights.setColor(trafficLights.getStare().next());
+        }
+    }
+    static void changeAll(){
+        for(int i=0; i< CityGenerator.city.getTrafficLights().size(); i++) {
+            TrafficLights trafficLights=CityGenerator.city.getTLightsById(i);
+            change(trafficLights);
+        }
+    }
+   /* void printAll(){
+        System.out.println("Dupa o secunda: ");
+        for(int i=0; i< CityGenerator.city.getTrafficLights().size(); i++){
+            System.out.println(CityGenerator.city.getTLightsById(i));
+        }
+    }
+
+    */
     @Override
     public void run() {
 
         while (existsACarOnStreets()){
-
-            for(int i=0; i< CityGenerator.city.getTrafficLights().size(); i++) {
-                TrafficLights trafficLights=CityGenerator.city.getTLightsById(i);
-                if (trafficLights.getTimer() > 0) {
-                    trafficLights.setTimer(trafficLights.getTimer() - 1);
-                } else {
-                    trafficLights.setTimer(trafficLights.getMaxTime());
-                    trafficLights.setColor(trafficLights.getStare().next());
-                }
-            }
+            changeAll();
             try {
-                sleep(1000);
+                sleep(100);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+               // e.printStackTrace();
             }
-
-            System.out.println("Dupa o secunda: ");
             for(int i=0; i< CityGenerator.city.getTrafficLights().size(); i++){
                 System.out.println(CityGenerator.city.getTLightsById(i));
             }
         }
-        System.out.println("semaphore controller run");
     }
 }
