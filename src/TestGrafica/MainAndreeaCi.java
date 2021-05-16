74a6168870074867957a44fb5df64cae501944a2
andreeabmanea <andreeamanea66@gmail.com>
2021-05-16 16:38:59 +0300

    direction of a car based on where it is

--- src/TestGrafica/MainAndreeaCi.java
+++ src/TestGrafica/MainAndreeaCi.java
@@ -40,11 +40,11 @@ public class MainAndreeaCi extends Application{
         //Pane canvas = new Pane();
         //Scene scene = new Scene(canvas, 900, 900, Color.ALICEBLUE);
         Image car = new Image("file:///C:\\Users\\andre\\OneDrive\\Desktop\\ip-vTest\\programming-engineering\\src\\TestGrafica\\carGoingRight.png");
-        ImageView ball = new ImageView(car);
+        ImageView carView = new ImageView(car);
 
-        putFirstCar(ball, ourCity);
+        putFirstCar(carView, ourCity);
 
-        stage.setTitle("Animated Ball");
+        stage.setTitle("Traffic Simulator");
         stage.setScene(scene);
         stage.show();
 
@@ -59,21 +59,29 @@ public class MainAndreeaCi extends Application{
                     @Override
                     public void handle(ActionEvent t) {
                         //move the ball
-                        System.out.println(ball.getLayoutX());
-                        System.out.println(ball.getLayoutY());
+                        System.out.println(carView.getLayoutX());
+                        System.out.println(carView.getLayoutY());
                        // ball.setLayoutX(ball.getLayoutX() + dx);
                        // ball.setLayoutY(ball.getLayoutY());
                        //
-                        if(CityGenerator.city.getStreetByIndex(CityGenerator.city.getCars().get(0).getCurrentPosition()).getDirection()==-1){
-                            var path = new File("src\\GraphicsModule\\resources\\carGoingUp.png").getAbsolutePath();
-                            ball.setImage(new Image("file:///" + path));
+                        if(CityGenerator.city.getStreetByIndex(CityGenerator.city.getCars().get(0).getCurrentPosition()).getDirection()==1){
+                            if (CityGenerator.city.getCars().get(0).getDirection() == 1) {
+                                var path = new File("src\\GraphicsModule\\resources\\carGoingRight.png").getAbsolutePath();
+                                carView.setImage(new Image("file:///" + path));
+                            } else {
+                                var path = new File("src\\GraphicsModule\\resources\\carGoingLeft.png").getAbsolutePath();
+                                carView.setImage(new Image("file:///" + path));
+                            }
 //                            ball.setImage(new Image("file:///Users\\andre\\OneDrive\\Desktop\\ip-try3\\programming-engineering\\src\\GraphicsModule\\resources\\carGoingUp.png"));
                         }
-                        else{
-                            var path = new File("src\\TestGrafica\\carGoingRight.png").getAbsolutePath();
-                            ball.setImage(new Image("file:///" + path));
-//                            ball.setImage(new Image("file:///C:\\Users\\andre\\OneDrive\\Desktop\\ip-vTest\\programming-engineering\\src\\TestGrafica\\carGoingRight.png"));
+                        else if (CityGenerator.city.getCars().get(0).getDirection() == 1) {
+                            var path = new File("src\\GraphicsModule\\resources\\carGoingDown.png").getAbsolutePath();
+                            carView.setImage(new Image("file:///" + path));
+                        } else {
+                            var path = new File("src\\GraphicsModule\\resources\\carGoingUp.png").getAbsolutePath();
+                            carView.setImage(new Image("file:///" + path));
 
+//                            ball.setImage(new Image("file:///C:\\Users\\andre\\OneDrive\\Desktop\\ip-vTest\\programming-engineering\\src\\TestGrafica\\carGoingRight.png"));
                         }
                         int x, y;
                         try {
@@ -85,8 +93,8 @@ public class MainAndreeaCi extends Application{
                             CarAnimator.rwLock.readLock().unlock();
                         }
                         System.out.println("AICI ESTE VALOAREA COORDONATELOR: " + x + " . " + y);
-                        ball.setLayoutX(y*100);
-                        ball.setLayoutY(x*100);
+                        carView.setLayoutX(y*100);
+                        carView.setLayoutY(x*100);
                         System.out.println("Aici este valoare trimisa de GetData actualizata: \t" );
 
                         for(int i=0; i< CityGenerator.city.getCars().size(); i++)
@@ -96,7 +104,7 @@ public class MainAndreeaCi extends Application{
                                             "\tAcesta distanta pe strada" + CityGenerator.city.getCars().get(i).getDistance() +
                                             "\n Current Position:"+CityGenerator.city.getCars().get(i).getCurrentPosition()
                             + ConsoleColors.RESET);
-                        System.out.println(ball.getLayoutX() + dx);
+                        System.out.println(carView.getLayoutX() + dx);
 
                     }
                 }));
@@ -114,22 +122,32 @@ public class MainAndreeaCi extends Application{
         System.out.println("am inceput");
         launch();
     }
-    public static void putFirstCar(ImageView ball, Graphics2 ourCity){
+    public static void putFirstCar(ImageView carView, Graphics2 ourCity){
         int x1= CityGenerator.city.getCarCoordinates(CityGenerator.city.getCars().get(0)).getKey();
 
         int y1 = CityGenerator.city.getCarCoordinates(CityGenerator.city.getCars().get(0)).getValue();
-        ball.setLayoutX(y1*100);
-        ball.setLayoutY(x1*100);
+        carView.setLayoutX(y1*100);
+        carView.setLayoutY(x1*100);
         if(!isVertical()) {
-            var path = new File("src\\TestGrafica\\carGoingRight.png").getAbsolutePath();
-            ball.setImage(new Image("file:///" + path));
+
+            if (CityGenerator.city.getCars().get(0).getDirection() == 1) {
+                var path = new File("src\\GraphicsModule\\resources\\carGoingRight.png").getAbsolutePath();
+                carView.setImage(new Image("file:///" + path));
+            } else {
+                var path = new File("src\\GraphicsModule\\resources\\carGoingLeft.png").getAbsolutePath();
+                carView.setImage(new Image("file:///" + path));
+            }
         }
-        else {
-            var path = new File("src\\GraphicsModule\\resources\\carGoingUp.png").getAbsolutePath();
-            ball.setImage(new Image("file:///" + path));
-        }
+        else if (CityGenerator.city.getCars().get(0).getDirection() == 1) {
+                var path = new File("src\\GraphicsModule\\resources\\carGoingDown.png").getAbsolutePath();
+                carView.setImage(new Image("file:///" + path));
+            } else {
+                var path = new File("src\\GraphicsModule\\resources\\carGoingUp.png").getAbsolutePath();
+                carView.setImage(new Image("file:///" + path));
+            }
+
         System.out.println(x1 + "..." + y1);
-        ourCity.window.getChildren().add(ball);
+        ourCity.window.getChildren().add(carView);
 
     }
     public static boolean isVertical(){
